a5f3452cdd2a0c3edc61a8e157a3ed88223ee14c
Erdi Chen <erdichen@gmail.com>
2022-01-12 04:59:19 -0800

    Fix test

--- evdev/keycode/bits_test.go
+++ evdev/keycode/bits_test.go
@@ -6,13 +6,13 @@ import (
 
 func TestKeyBits(t *testing.T) {
 	var keys KeyBits
-	for k := Key_KEY_ESC; k < Key_KEY_CNT; k++ {
+	for k := Code_KEY_ESC; k < Code_KEY_CNT; k++ {
 		keys.Set(k, true)
 		if got := keys.Get(k); !got {
 			t.Errorf("Set key %v got %v want %v", k, got, true)
 		}
 	}
-	for k := Key_KEY_ESC; k < Key_KEY_CNT; k++ {
+	for k := Code_KEY_ESC; k < Code_KEY_CNT; k++ {
 		keys.Set(k, false)
 		if got := keys.Get(k); got {
 			t.Errorf("Set key %v got %v want %v", k, got, false)
